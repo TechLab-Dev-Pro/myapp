1c1148653134ecd01fb4623a191a71721aa2bfa6
TechLab-Dev-Pro <TechLab-Dev@proton.me>
2026-04-03 14:34:58 +0200

    Enhance GUI with exit button and input validation
    Added exit button and improved error handling for input.

--- pythonProject/CE_3.py
+++ pythonProject/CE_3.py
@@ -1,6 +1,8 @@
 import FreeSimpleGUI as sg
 from converters14 import convert
 
+sg.theme("Black")
+
 label1 = sg.Text("Enter feet:")
 input1 = sg.Input(key="feet")
 
@@ -9,20 +11,29 @@ input2 = sg.Input(key="inches")
 
 convert_button = sg.Button("Convert")
 output_label = sg.Text(key="output", text_color="black")
-
+exit_button = sg.Button("Exit")
 
 window = sg.Window("Convertor",
                    layout=[[label1, input1],
                            [label2, input2],
-                           [convert_button, output_label]])
+                           [convert_button, exit_button, output_label]])
 
 while True:
     event, values = window.read()
-    feet = float(values["feet"])
-    inches = float(values["inches"])
+    match event:
+        case "Exit":
+            break
+        case sg.WINDOW_CLOSED:
+            break
 
-    result = convert(feet, inches)
-    window["output"].update(value=f"{result} m", text_color="white")
+    try:
+        feet = float(values["feet"])
+        inches = float(values["inches"])
 
+        result = convert(feet, inches)
+        window["output"].update(value=f"{result} m", text_color="white")
+
+    except ValueError:
+        sg.popup("Please provide two numbers.", font=("Helvetica", 20))
 
 window.close()
