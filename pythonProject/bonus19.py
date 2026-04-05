70bd96b364b5378b397d49e2f337302ed75232d4
TechLab-Dev-Pro <258237329+TechLab-Dev-Pro@users.noreply.github.com>
2026-04-05 13:57:17 +0200

    Implement color to grayscale image converter
    Added functionality to convert uploaded or camera images to grayscale.

---
+++ pythonProject/bonus19.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from PIL import Image
+
+
+st.subheader("Color to Grayscale Converter")
+
+uploaded_image = st.file_uploader("Upload Image")
+
+with st.expander("Start Camera"):
+    # Start the camera
+    camera_image = st.camera_input("Camera")
+
+
+if camera_image:
+    # Create a pillow image instance
+    img = Image.open(camera_image)
+
+    #Convert the pillow image to grayscale
+    gray_img = img.convert('L')
+
+    # Render the grayscale image on the webpage
+    st.image(gray_img)
+
+if uploaded_image:
+    img = Image.open(uploaded_image)
+    gray_img = img.convert('L')
+    st.image(gray_img)
